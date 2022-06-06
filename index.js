1c139d393919bc3f4df064fda2679e338ee20fa7
X_Niter <drumsforlife1999@yahoo.com>
2022-06-06 02:07:02 -0500

    New Whitelist,guildMemberRemove working

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 require('dotenv').config()
 require('./register-commands')
 const fs = require('fs')
-const { Client, Collection, Intents } = require("discord.js")
-const client = new Client({ intents: [Intents.FLAGS.GUILD_PRESENCES,Intents.FLAGS.GUILD_MESSAGES,Intents.FLAGS.GUILD_MEMBERS,Intents.FLAGS.GUILDS,Intents.FLAGS.DIRECT_MESSAGES] , partials: ['CHANNEL']});
+const {
+    Client,
+    Collection,
+    Intents,
+    MessageActionRow,
+    MessageSelectMenu,
+    MessageEmbed,
+    Message
+} = require("discord.js")
+const client = new Client({
+    intents: [Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES],
+    partials: ['CHANNEL']
+});
+
 //make a collection of commands in the client, scan for commands and for each commands that ends in .js add to the collection
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -10,7 +22,11 @@ const db = require('better-sqlite3')('users.db');
 const axios = require('axios').default;
 // Global API Reference for the whole class
 const API = `${process.env.AMPIP}/API`;
-const {getInstance, sendToInstance} = require('./ampWrapper.js')
+
+const {
+    getInstance,
+    sendToInstance
+} = require('./ampWrapper.js')
 
 //initialize log file if it doesn't already exist
 if (!fs.existsSync('./log.txt')) {
@@ -26,15 +42,13 @@ process.on('uncaughtException', (err) => {
 const source = axios.CancelToken.source();
 //axios timeout request in case an infinite yeld on a api request
 const timeout = setTimeout(() => {
-  source.cancel();
-  // Timeout Logic
-}, 15*1000);
-
-//FIXME code for removing users from the whitelist when they leave the server is no longer here, add it back when you find a possible solution
+    source.cancel();
+    // Timeout Logic
+}, 15 * 1000);
 
 
-//i will still store the user id in the database however
-async function insertToDb(queryString){
+//Store user data in the sqlite database
+async function insertToDb(queryString) {
     let query = await db.prepare(queryString).run()
     return query
 }
@@ -49,28 +63,54 @@ for (const file of commandFiles) {
     client.commands.set(command.data.name, command);
 }
 
-client.on("ready", async () => {
-    console.log("bot is ready to roll")
+// .json server list parser/constructor
+function constructJSON() {
+    let servers
+    servers = JSON.parse(fs.readFileSync('./servers.json', 'utf8'))
+    servers.forEach(server => {
+        server.value = server.value + "," + server.label
+    })
+    return servers
+}
+
+client.on("ready", async() => {
+    const channel = client.channels.cache.get(`${process.env.MenuChannelID}`);
+    const embed = new MessageEmbed().setTitle("Select the server you wish to be white listed on");
+
+    setTimeout(() => Message.delete(), 1);
+
+    const row = new MessageActionRow().addComponents(
+        new MessageSelectMenu()
+        .setCustomId("whitelist")
+        .setPlaceholder("Select a server")
+        .addOptions(constructJSON())
+    )
+    channel.send({
+        embeds: [embed],
+        components: [row]
+    })
+    console.warn("Devious Whitelister Discord Bot Copyright (C) 2022 By:M1so/X_Niter \n (GNU GENERAL PUBLIC LICENSE)[Version 3, 29 June 2007] \n This program comes with ABSOLUTELY NO WARRANTY; \n This is free software, and you are welcome to do as you please under one condition, \n Proper credit be given by documenting our names for the work we have done.");
+    console.info("Whitelist Bot loaded Succesfully")
 })
 
-//UNTESTED CODE
-//----------------------------------------------
+//User leaves discord, remove them from whitelist, DB, and kick them from server just incase they are on it
 client.on('guildMemberRemove', async member => {
-    function findInJson(instanceName){
-        let servers = JSON.parse(fs.readFileSync('./servers.json'))
-        let server = servers.find(server => server.value.toString().split(',')[0] === instanceName)
-        return server
-    }
     let users = await retrieveFromDb(`SELECT * FROM users WHERE id = '${member.id}'`)
-    users.forEach(async (user) => {
-        let api = findInJson(user.server)
-        let GUID = await getInstance(user.server, api)
-        await sendToInstance(GUID, `whitelist remove ${user.name}`,api)
-    })
-    
-    await insertToDb(`DELETE FROM users WHERE id = '${member.id}'`)
+
+    try {
+        users.forEach(async(user) => {
+            let GUID = await getInstance(user.server, user.api)
+
+            await sendToInstance(GUID, `whitelist remove ${user.name}`, user.api)
+            await sendToInstance(GUID, `kick ${user.name} User left Discord Community`, user.api)
+        })
+
+        await insertToDb(`DELETE FROM users WHERE id = '${member.id}'`)
+    } catch (error) {
+        console.error(error);
+        console.error("Error removing user from Devious Network index.js 71-78");
+    }
 })
-//----------------------------------------------
 
 client.on('interactionCreate', async interaction => {
     //interaction executer and error handling
@@ -87,18 +127,22 @@ client.on('interactionCreate', async interaction => {
             console.error(error);
             //log to file
             fs.appendFileSync('./log.txt', `${new Date().toLocaleString()}: ${error} in index.js at line 151\n`)
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+            await interaction.reply({
+                content: 'There was an error while executing this command!',
+                ephemeral: true
+            });
         }
     }
-    if(interaction.isSelectMenu()) {
+    if (interaction.isSelectMenu()) {
         try {
             const command = client.commands.get(interaction.customId)
             command.onSelect(interaction);
         } catch (error) {
             //log to file
-            fs.appendFileSync('./log.txt', `${new Date().toLocaleString()}: ${error} in index.js at line 161\n`)
+            fs.appendFileSync('./log.txt', `${new Date().toLocaleString()}: ${error} in index.js at line 96\n`)
             console.log(error);
         }
     }
 })
-client.login(process.env.DISCORD_TOKEN) 
+
+client.login(process.env.DISCORD_TOKEN)
