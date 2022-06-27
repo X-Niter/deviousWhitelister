cde9b2ab695f925c27bcaa5c51e16cff2678b2ad
X_Niter <drumsforlife1999@yahoo.com>
2022-06-27 15:22:43 -0500

    Faster cmd execution, single or multiple commands functioning, #tested

--- ampWrapper.js
+++ ampWrapper.js
@@ -79,18 +79,31 @@ async function sendToInstance(GUID, messages, API) {
 
             instanceSessionId = instanceSessionId.data.sessionID
             //check if messages is an array, if it is an array then for each message in the array we will await axios.post(API + `/ADSModule/Servers/${GUID}/API/ADSModule/SendConsoleMessage`, { message: message, SESSIONID: instanceSessionId, cancelToken: source.token }) and export the axios post as a variable so we can return it as response.data.result
-            if (Array.isArray(messages)) {
+            // if (Array.isArray(messages)) {
+            //     clearTimeout(timeout);
+            //     console.log(`Sending commands to AMP server`);
+            //     messages.forEach(function (message) {
+            //         let response = await axios.post(API + `/ADSModule/Servers/${GUID}/API/Core/SendConsoleMessage`, { message: messages, SESSIONID: instanceSessionId, cancelToken: source.token })
+            //         return response.data
+            //     });
+
+            //     //let response = await Promise.all(messages.map(message => axios.post(API + `/ADSModule/Servers/${GUID}/API/Core/SendConsoleMessage`, { message: message, SESSIONID: instanceSessionId, cancelToken: source.token })))
+            //     //return response.data
+            // } else {
+            //     clearTimeout(timeout);
+            //     console.log(`Sending command to AMP server`);
+            //     //if messages is not an array then we will just await axios.post(API + `/ADSModule/Servers/${GUID}/API/ADSModule/SendConsoleMessage`, { message: message, SESSIONID: instanceSessionId, cancelToken: source.token }) and export the axios post as a variable so we can return it as response.data.result
+            //     let response = await axios.post(API + `/ADSModule/Servers/${GUID}/API/Core/SendConsoleMessage`, { message: messages, SESSIONID: instanceSessionId, cancelToken: source.token })
+            //     return response.data
+            // }     
+            if (messages != null) {
                 clearTimeout(timeout);
                 console.log(`Sending commands to AMP server`);
-                let response = await Promise.all(messages.map(message => axios.post(API + `/ADSModule/Servers/${GUID}/API/Core/SendConsoleMessage`, { message: message, SESSIONID: instanceSessionId, cancelToken: source.token })))
-                return response.data
-            } else {
-                clearTimeout(timeout);
-                console.log(`Sending command to AMP server`);
-                //if messages is not an array then we will just await axios.post(API + `/ADSModule/Servers/${GUID}/API/ADSModule/SendConsoleMessage`, { message: message, SESSIONID: instanceSessionId, cancelToken: source.token }) and export the axios post as a variable so we can return it as response.data.result
-                let response = await axios.post(API + `/ADSModule/Servers/${GUID}/API/Core/SendConsoleMessage`, { message: messages, SESSIONID: instanceSessionId, cancelToken: source.token })
-                return response.data
-            }            
+                messages.forEach(async message => {
+                    let response = await axios.post(API + `/ADSModule/Servers/${GUID}/API/Core/SendConsoleMessage`, { message: message, SESSIONID: instanceSessionId, cancelToken: source.token })
+                    return response.data
+                });
+            }       
             
         } else {
             clearTimeout(timeout);
